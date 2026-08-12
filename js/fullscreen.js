a302797cbe02ca42e625b27c5441cd3daf29309f
Xalsier <81521840+Xalsier@users.noreply.github.com>
2026-08-12 18:57:07 -0400

    rm fullscreen button

--- js/fullscreen.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('fullscreenBtn').addEventListener('click', (e) => {
-    e.stopPropagation();
-    if (!document.fullscreenElement) { document.documentElement.requestFullscreen().catch(() => {});
-    } else { if (document.exitFullscreen) document.exitFullscreen(); }
-});
